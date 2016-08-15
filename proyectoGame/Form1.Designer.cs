ce757810b51b1b5d01fde2e63707ec09a08b21e1
Jesus M. Tellez <jesus_8729@hotmail.com>
2016-08-15 12:44:00 -0700

    juego de posiciones y colores

---
+++ proyectoGame/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace proyectoGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNeutralColor = new System.Windows.Forms.Label();
+            this.lblErrorColor = new System.Windows.Forms.Label();
+            this.lblAciertoColor = new System.Windows.Forms.Label();
+            this.lblNeutral = new System.Windows.Forms.Label();
+            this.lblFallos = new System.Windows.Forms.Label();
+            this.lblAciertos = new System.Windows.Forms.Label();
+            this.lblTrial = new System.Windows.Forms.Label();
+            this.lblColorMatch = new System.Windows.Forms.Label();
+            this.lblPositionMatch = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblNeutralColor
+            // 
+            this.lblNeutralColor.AutoSize = true;
+            this.lblNeutralColor.Location = new System.Drawing.Point(397, 142);
+            this.lblNeutralColor.Name = "lblNeutralColor";
+            this.lblNeutralColor.Size = new System.Drawing.Size(13, 13);
+            this.lblNeutralColor.TabIndex = 59;
+            this.lblNeutralColor.Text = "0";
+            // 
+            // lblErrorColor
+            // 
+            this.lblErrorColor.AutoSize = true;
+            this.lblErrorColor.ForeColor = System.Drawing.Color.Red;
+            this.lblErrorColor.Location = new System.Drawing.Point(397, 258);
+            this.lblErrorColor.Name = "lblErrorColor";
+            this.lblErrorColor.Size = new System.Drawing.Size(13, 13);
+            this.lblErrorColor.TabIndex = 58;
+            this.lblErrorColor.Text = "0";
+            // 
+            // lblAciertoColor
+            // 
+            this.lblAciertoColor.AutoSize = true;
+            this.lblAciertoColor.ForeColor = System.Drawing.Color.Green;
+            this.lblAciertoColor.Location = new System.Drawing.Point(398, 42);
+            this.lblAciertoColor.Name = "lblAciertoColor";
+            this.lblAciertoColor.Size = new System.Drawing.Size(13, 13);
+            this.lblAciertoColor.TabIndex = 57;
+            this.lblAciertoColor.Text = "0";
+            // 
+            // lblNeutral
+            // 
+            this.lblNeutral.AutoSize = true;
+            this.lblNeutral.Location = new System.Drawing.Point(18, 142);
+            this.lblNeutral.Name = "lblNeutral";
+            this.lblNeutral.Size = new System.Drawing.Size(13, 13);
+            this.lblNeutral.TabIndex = 52;
+            this.lblNeutral.Text = "0";
+            // 
+            // lblFallos
+            // 
+            this.lblFallos.AutoSize = true;
+            this.lblFallos.ForeColor = System.Drawing.Color.Red;
+            this.lblFallos.Location = new System.Drawing.Point(18, 258);
+            this.lblFallos.Name = "lblFallos";
+            this.lblFallos.Size = new System.Drawing.Size(13, 13);
+            this.lblFallos.TabIndex = 51;
+            this.lblFallos.Text = "0";
+            // 
+            // lblAciertos
+            // 
+            this.lblAciertos.AutoSize = true;
+            this.lblAciertos.ForeColor = System.Drawing.Color.Green;
+            this.lblAciertos.Location = new System.Drawing.Point(19, 42);
+            this.lblAciertos.Name = "lblAciertos";
+            this.lblAciertos.Size = new System.Drawing.Size(13, 13);
+            this.lblAciertos.TabIndex = 50;
+            this.lblAciertos.Text = "0";
+            // 
+            // lblTrial
+            // 
+            this.lblTrial.AutoSize = true;
+            this.lblTrial.Location = new System.Drawing.Point(211, 13);
+            this.lblTrial.Name = "lblTrial";
+            this.lblTrial.Size = new System.Drawing.Size(13, 13);
+            this.lblTrial.TabIndex = 49;
+            this.lblTrial.Text = "0";
+            // 
+            // lblColorMatch
+            // 
+            this.lblColorMatch.AutoSize = true;
+            this.lblColorMatch.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblColorMatch.Location = new System.Drawing.Point(269, 307);
+            this.lblColorMatch.Name = "lblColorMatch";
+            this.lblColorMatch.Size = new System.Drawing.Size(167, 29);
+            this.lblColorMatch.TabIndex = 46;
+            this.lblColorMatch.Text = "L: Color Match";
+            // 
+            // lblPositionMatch
+            // 
+            this.lblPositionMatch.AutoSize = true;
+            this.lblPositionMatch.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPositionMatch.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblPositionMatch.Location = new System.Drawing.Point(12, 307);
+            this.lblPositionMatch.Name = "lblPositionMatch";
+            this.lblPositionMatch.Size = new System.Drawing.Size(197, 29);
+            this.lblPositionMatch.TabIndex = 45;
+            this.lblPositionMatch.Text = "A: Position Match";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Location = new System.Drawing.Point(65, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(307, 252);
+            this.groupBox1.TabIndex = 44;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Game";
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.Transparent;
+            this.button2.Location = new System.Drawing.Point(155, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 110);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.Color.Transparent;
+            this.button4.Location = new System.Drawing.Point(155, 135);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(143, 110);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(6, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 110);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.Transparent;
+            this.button3.Location = new System.Drawing.Point(7, 135);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(143, 110);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 347);
+            this.Controls.Add(this.lblNeutralColor);
+            this.Controls.Add(this.lblErrorColor);
+            this.Controls.Add(this.lblAciertoColor);
+            this.Controls.Add(this.lblNeutral);
+            this.Controls.Add(this.lblFallos);
+            this.Controls.Add(this.lblAciertos);
+            this.Controls.Add(this.lblTrial);
+            this.Controls.Add(this.lblColorMatch);
+            this.Controls.Add(this.lblPositionMatch);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Game";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNeutralColor;
+        private System.Windows.Forms.Label lblErrorColor;
+        private System.Windows.Forms.Label lblAciertoColor;
+        private System.Windows.Forms.Label lblNeutral;
+        private System.Windows.Forms.Label lblFallos;
+        private System.Windows.Forms.Label lblAciertos;
+        private System.Windows.Forms.Label lblTrial;
+        private System.Windows.Forms.Label lblColorMatch;
+        private System.Windows.Forms.Label lblPositionMatch;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ proyectoGame/Form1.cs
@@ -0,0 +1,440 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proyectoGame
+{
+    public partial class Form1 : Form
+    {
+        int[] trials = new int[20];
+        
+        int[] trialsColors = new int[20];
+        
+        int currentTrial = 0;
+        
+        bool waitingResp = false;
+        
+        bool isMatch = false;
+        
+        bool isMatchColor = false;
+        
+        bool inputTriggered = false;
+        
+        bool inputTriggeredColor = false;
+        
+        Timer timerBetween, timerResponse;
+
+        int aciertos = 0, errores = 0, neutral = 0, aciertosColor = 0, erroresColor = 0, neutralColor = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            initTrials(trials, 1, 4);
+
+            initTrials(trialsColors, 1, 4);
+
+
+            initializeTimers();
+
+            declareKeydowns(this.button1);
+            declareKeydowns(this.button2);
+            declareKeydowns(this.button3);
+            declareKeydowns(this.button4);
+
+            timerBetween.Start();
+        }
+        
+        private void restarBooleans()
+        {
+            isMatch = false;
+            waitingResp = false;
+            inputTriggered = false;
+
+            isMatchColor = false;
+
+            inputTriggeredColor = false;
+        }
+        
+        private void restarPositions()
+        {
+            button1.BackColor = Color.Transparent;
+            button2.BackColor = Color.Transparent;
+            button3.BackColor = Color.Transparent;
+            button4.BackColor = Color.Transparent;
+
+            lblTrial.Text = (currentTrial + 1).ToString();
+
+            lblPositionMatch.ForeColor = Color.Black;
+            lblColorMatch.ForeColor = Color.Black;
+        }
+        
+        private void setPosition(int _currentTrial, int[] _trials, int[] _trialsColors)
+        {
+            int positionTrial = _trials[_currentTrial];
+
+            int positionTrialColor = _trialsColors[_currentTrial];
+
+            switch (positionTrial)
+            {
+                case 1:
+                    setterColorTrial(button1, positionTrialColor);
+                    break;
+                case 2:
+                    setterColorTrial(button2, positionTrialColor);
+                    break;
+                case 3:
+                    setterColorTrial(button3, positionTrialColor);
+                    break;
+                case 4:
+                    setterColorTrial(button4, positionTrialColor);
+                    break;
+            }
+        }
+
+        private void setterColorTrial(Button _btn, int _positionTrialColor)
+        {
+            switch (_positionTrialColor)
+            {
+                case 1:
+                    _btn.BackColor = Color.Red;
+                    break;
+                case 2:
+                    _btn.BackColor = Color.Green;
+                    break;
+                case 3:
+                    _btn.BackColor = Color.Blue;
+                    break;
+                case 4:
+                    _btn.BackColor = Color.Yellow;
+                    break;
+            }
+        }
+        
+        private void updateScore()
+        {
+            lblAciertos.Text = aciertos.ToString();
+            lblNeutral.Text = neutral.ToString();
+            lblFallos.Text = errores.ToString();
+
+            lblAciertoColor.Text = aciertosColor.ToString();
+            lblNeutralColor.Text = neutralColor.ToString();
+            lblErrorColor.Text = erroresColor.ToString();
+
+        }
+        
+        public void declareKeydowns(Button _buton)
+        {
+            _buton.KeyDown += new KeyEventHandler(this.allbutons_KeyDowns);
+        }
+        
+        private void allbutons_KeyDowns(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.A && waitingResp == true && inputTriggered == false)
+            {
+                inputTriggered = true;
+
+                isMatch = true;
+
+                if (currentTrial <= 19)
+                {
+                    printValidateTrial();
+                }
+            }
+
+            if (e.KeyCode == Keys.L && waitingResp == true && inputTriggeredColor == false)
+            {
+                inputTriggeredColor = true;
+
+                isMatchColor = true;
+
+                if (currentTrial <= 19)
+                {
+                    printValidateColorTrial();
+                }
+            }
+        }
+
+        public static void initTrials(int[] _trials, int _range1, int _range2)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 20; i++)
+            {
+                _trials[i] = rnd.Next(_range1, _range2);
+            }
+        }
+
+        public void trialsStatics(int[] _trials)
+        {
+            _trials[0] = 2;
+            _trials[1] = 2;
+            _trials[2] = 1;
+            _trials[3] = 2;
+            _trials[4] = 3;
+            _trials[5] = 2;
+            _trials[6] = 3;
+            _trials[7] = 3;
+            _trials[8] = 1;
+            _trials[9] = 2;
+            _trials[10] = 1;
+            _trials[11] = 2;
+            _trials[12] = 1;
+            _trials[13] = 4;
+            _trials[14] = 1;
+            _trials[15] = 2;
+            _trials[16] = 4;
+            _trials[17] = 2;
+            _trials[18] = 4;
+            _trials[19] = 4;
+
+        }
+        
+        public void trialsColorStatics(int[] _trialsColors)
+        {
+            _trialsColors[0] = 2;
+            _trialsColors[1] = 2;
+            _trialsColors[2] = 1;
+            _trialsColors[3] = 2;
+            _trialsColors[4] = 3;
+            _trialsColors[5] = 2;
+            _trialsColors[6] = 3;
+            _trialsColors[7] = 3;
+            _trialsColors[8] = 1;
+            _trialsColors[9] = 2;
+            _trialsColors[10] = 1;
+            _trialsColors[11] = 2;
+            _trialsColors[12] = 1;
+            _trialsColors[13] = 4;
+            _trialsColors[14] = 1;
+            _trialsColors[15] = 2;
+            _trialsColors[16] = 4;
+            _trialsColors[17] = 2;
+            _trialsColors[18] = 4;
+            _trialsColors[19] = 4;
+        }
+        
+        private void initializeTimers()
+        {
+            timerBetween = new Timer();
+            timerResponse = new Timer();
+
+            timerBetween.Interval = 1000;
+            timerBetween.Tick += timerBetween_Tick;
+
+            timerResponse.Interval = 1000;
+            timerResponse.Tick += timerResponse_Tick;
+
+        }
+        
+        private void timerBetween_Tick(object sender, EventArgs e)
+        {
+            timerBetween.Stop();
+
+            waitingResp = true;
+
+            timerResponse.Start();
+
+            if (currentTrial >= 0 && currentTrial <= 19)
+            {
+                setPosition(currentTrial, trials, trialsColors);
+            }
+        }
+        
+        private void timerResponse_Tick(object sender, EventArgs e)
+        {
+            
+            if (currentTrial <= 19)
+            {
+                validateTrial();
+
+                validateTrialColor();
+
+                timerResponse.Stop();
+
+                restarPositions();
+
+                updateScore();
+
+                restarBooleans();
+
+                timerBetween.Start();
+
+                currentTrial++;
+            }
+
+        }
+        
+        private void validateTrial()
+        {
+            if (isMatch == true)
+            {
+                if (solveCurrentTrial() == true)
+                {
+                    aciertos++;
+                }
+                else
+                {
+                    errores++;
+                }
+            }
+            else
+            {
+                if (solveCurrentTrial() == true)
+                {
+                    errores++;
+                }
+                else
+                {
+                    neutral++;
+                }
+            }
+        }
+        
+        private void validateTrialColor()
+        {
+            if (isMatchColor == true)
+            {
+                if (solveCurrenTrialColor() == true)
+                {
+                    aciertosColor++;
+                }
+                else
+                {
+                    erroresColor++;
+                }
+            }
+            else
+            {
+                if (solveCurrenTrialColor() == true)
+                {
+                    erroresColor++;
+                }
+                else
+                {
+                    neutralColor++;
+                }
+            }
+        }
+        
+        private void printValidateTrial()
+        {
+            if (isMatch == true)
+            {
+                if (solveCurrentTrial() == true)
+                {
+                    lblPositionMatch.ForeColor = Color.Green;
+                }
+                else
+                {
+                    
+                    lblPositionMatch.ForeColor = Color.Red;
+                }
+            }
+            else
+            {
+                if (solveCurrentTrial() == true)
+                {
+                    lblPositionMatch.ForeColor = Color.Red;
+                }
+                else
+                {
+                    lblPositionMatch.ForeColor = Color.Black;
+                }
+            }
+        }
+        
+        private void printValidateColorTrial()
+        {
+            if (isMatchColor == true)
+            {
+                if (solveCurrenTrialColor() == true)
+                {
+                    lblColorMatch.ForeColor = Color.Green;
+                }
+                else
+                {
+                    lblColorMatch.ForeColor = Color.Red;
+                }
+            }
+            else
+            {
+                if (solveCurrenTrialColor() == true)
+                {
+                    lblColorMatch.ForeColor = Color.Red;
+                }
+                else
+                {
+                    lblColorMatch.ForeColor = Color.Black;
+                }
+            }
+        }
+        
+        private bool solveCurrentTrial()
+        {
+            int antTrialValue;
+            int currTrialValue;
+
+            if (currentTrial == 20)
+            {
+                antTrialValue = trials[(currentTrial - 2)];
+                currTrialValue = trials[(currentTrial - 1)];
+            }
+            else if (currentTrial >= 1 && currentTrial <= 19)
+            {
+                antTrialValue = trials[(currentTrial - 1)];
+                currTrialValue = trials[(currentTrial)];
+            }
+            else
+            {
+                return false;
+            }
+
+
+            if (antTrialValue == currTrialValue)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+        private bool solveCurrenTrialColor()
+        {
+            int antTrialValue, currTrialValue;
+
+            if (currentTrial == 20)
+            {
+                antTrialValue = trialsColors[(currentTrial - 2)];
+                currTrialValue = trialsColors[(currentTrial - 1)];
+            }
+            else if (currentTrial >= 1 && currentTrial <= 19)
+            {
+                antTrialValue = trialsColors[(currentTrial - 1)];
+                currTrialValue = trialsColors[(currentTrial)];
+            }
+            else
+            {
+                return false;
+            }
+
+
+            if (antTrialValue == currTrialValue)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
